8c2f79273c3dcd9dcdf2e05d859b41529c6f84c6
Denys (thenester) Nesterenko <denys@thenester.co>
2024-06-28 11:15:32 +0300

    Initial commit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,10 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+map("n", "<leader>M", ":Mason<cr>", { desc = "Mason" })
+map("n", "<leader>G", ":LazyGit<cr>", { desc = "LazyGit" })
+map("n", "<leader><tab>", ":NvimTreeToggle<cr>", { desc = "NvimTree" })
+map("n", "<leader>fls", ":Telescope lsp_document_symbols<cr>", { desc = "telescope find symbols" })
+map("n", "<leader>flf", ":Telescope lsp_document_symbols symbols=function<cr>", { desc = "telescope find functions" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,31 +8,52 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server", "stylua",
+        "html-lsp", "css-lsp", "prettier",
+        "python-lsp-server", "gopls", "zls"
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "c"
+      },
+    },
+  },
+
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup {}
+    end
+  }
 }
